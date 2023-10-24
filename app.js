7c37828ab7e6b30d71761e8fcebd8f59ad4d7051
Nicole Reed <nickreed033@gmail.com>
2023-10-24 21:47:37 +0700

    copying to see if it fixes bug

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
+// JS file for cycleTracker app
+
+// -------------
+// Variable declarations
+// -------------
 const newPeriodFormEl = document.getElementsByTagName("form")[0];
 const startDateInputEl = document.getElementById("start-date");
 const endDateInputEl = document.getElementById("end-date");
 const pastPeriodContainer = document.getElementById("past-periods");
 
-// Add the storage key as an app-wide constant
+// Storage key is an app-wide constant
 const STORAGE_KEY = "period-tracker";
 
-// Listen to form submissions.
+// -------------
+// Event Handlers
+// -------------
 newPeriodFormEl.addEventListener("submit", (event) => {
     event.preventDefault();
     const startDate = startDateInputEl.value;
@@ -19,6 +26,11 @@ newPeriodFormEl.addEventListener("submit", (event) => {
     newPeriodFormEl.reset();
 });
 
+// -------------
+// Functionality
+// -------------
+
+// 1. Form validation
 function checkDatesInvalid(startDate, endDate) {
     if (!startDate || !endDate || startDate > endDate) {
         newPeriodFormEl.reset();
@@ -27,6 +39,7 @@ function checkDatesInvalid(startDate, endDate) {
     return false;
 }
 
+// 2. Get, add, sort, and store data
 function storeNewPeriod(startDate, endDate) {
     const periods = getAllStoredPeriods();
     periods.push({ startDate, endDate });
@@ -36,14 +49,14 @@ function storeNewPeriod(startDate, endDate) {
     window.localStorage.setItem(STORAGE_KEY, JSON.stringify(periods));
 }
 
+// 3. Get and parse data
 function getAllStoredPeriods() {
     const data = window.localStorage.getItem(STORAGE_KEY);
     const periods = data ? JSON.parse(data) : [];
-    console.dir(periods);
-    console.log(periods);
     return periods;
 }
 
+// 4. Display data
 function renderPastPeriods() {
     const pastPeriodHeader = document.createElement("h2");
     const pastPeriodList = document.createElement("ul");
@@ -65,9 +78,90 @@ function renderPastPeriods() {
     pastPeriodContainer.appendChild(pastPeriodList);
 }
 
+// 5. format dates for display
 function formatDate(dateString) {
     const date = new Date(dateString);
     return date.toLocaleDateString("en-US", { timeZone: "UTC" });
 }
 
+// -------------
+// Call render on page load
+// -------------
+
 renderPastPeriods();
+
+
+
+// const newPeriodFormEl = document.getElementsByTagName("form")[0];
+// const startDateInputEl = document.getElementById("start-date");
+// const endDateInputEl = document.getElementById("end-date");
+// const pastPeriodContainer = document.getElementById("past-periods");
+
+// // Add the storage key as an app-wide constant
+// const STORAGE_KEY = "period-tracker";
+
+// // Listen to form submissions.
+// newPeriodFormEl.addEventListener("submit", (event) => {
+//     event.preventDefault();
+//     const startDate = startDateInputEl.value;
+//     const endDate = endDateInputEl.value;
+//     if (checkDatesInvalid(startDate, endDate)) {
+//         return;
+//     }
+//     storeNewPeriod(startDate, endDate);
+//     renderPastPeriods();
+//     newPeriodFormEl.reset();
+// });
+
+// function checkDatesInvalid(startDate, endDate) {
+//     if (!startDate || !endDate || startDate > endDate) {
+//         newPeriodFormEl.reset();
+//         return true;
+//     }
+//     return false;
+// }
+
+// function storeNewPeriod(startDate, endDate) {
+//     const periods = getAllStoredPeriods();
+//     periods.push({ startDate, endDate });
+//     periods.sort((a, b) => {
+//         return new Date(b.startDate) - new Date(a.startDate);
+//     });
+//     window.localStorage.setItem(STORAGE_KEY, JSON.stringify(periods));
+// }
+
+// function getAllStoredPeriods() {
+//     const data = window.localStorage.getItem(STORAGE_KEY);
+//     const periods = data ? JSON.parse(data) : [];
+//     console.dir(periods);
+//     console.log(periods);
+//     return periods;
+// }
+
+// function renderPastPeriods() {
+//     const pastPeriodHeader = document.createElement("h2");
+//     const pastPeriodList = document.createElement("ul");
+//     const periods = getAllStoredPeriods();
+//     if (periods.length === 0) {
+//         return;
+//     }
+//     pastPeriodContainer.innerHTML = "";
+//     pastPeriodHeader.textContent = "Past periods";
+//     periods.forEach((period) => {
+//         const periodEl = document.createElement("li");
+//         periodEl.textContent = `From ${formatDate(
+//             period.startDate,
+//         )} to ${formatDate(period.endDate)}`;
+//         pastPeriodList.appendChild(periodEl);
+//     });
+
+//     pastPeriodContainer.appendChild(pastPeriodHeader);
+//     pastPeriodContainer.appendChild(pastPeriodList);
+// }
+
+// function formatDate(dateString) {
+//     const date = new Date(dateString);
+//     return date.toLocaleDateString("en-US", { timeZone: "UTC" });
+// }
+
+// renderPastPeriods();
